eda57b33d355d1f6032d7490da0ff7ad1ad9ef0c
geilsonfreire <geilsonfreireleite@gmail.com>
2024-04-12 18:39:12 -0300

    Foi aplicado a implementação dos eventos de mouse em script e aplicado efeitos de trnsição em css

--- src/Components/ContainerHeader.jsx
+++ src/Components/ContainerHeader.jsx
@@ -1,33 +1,62 @@
-import React from "react"; // Import React
+import React, { useEffect } from "react"; // Import React
 import "./ContainerHeader.css"; // Import CSS
 import { BiSearchAlt2 } from "react-icons/bi"; // Import IconName from react-icons/bi
 import { FaBell, FaChevronDown } from "react-icons/fa"; // Import IconName from react-icons/fa
 import Geilson from "../Assets/Geilson.png"; // Import Image from Geilson.png
 
+
 // Create a functional component called ContainerHeader
 function ContainerHeader() {
-  return (
-    <div className="ContainerHeader">
-      <div className="InputBox">
-        <input type="text" placeholder="Pesaquise colleções" />
-        <i className="SearchIcon"><BiSearchAlt2 /></i>
-      </div> {/* Container do imput */}
 
-      <div className="PerfilConatiner">
+    useEffect(() => { // UseEffect to open and close the drop menu
+        // Get the elements by ID   
+        const MenuTarget = document.getElementById('MenuChevron');
+        const MenuContainer = document.getElementById('MenuConteiner');
 
-            <i className="NotifiqueIcon"><FaBell /></i>
+        // Add the event listener to the element
+        MenuTarget.addEventListener("mouseenter", () => {
+            MenuTarget.style.transform = "rotate(180deg)";
+            MenuContainer.style.transform = "translateX(0px)";
+        }); // Evento de mouse houver para abrir o menu dropdown
+        
+        MenuContainer.addEventListener("mouseleave", () => {
+            MenuTarget.style.transform = "rotate(0deg)";
+            MenuContainer.style.transform = "translateX(300px)";
+        }); // Evento de mouse houver para fechar o menu dropdown
+        
+    }, []);
 
-            <div className="PerfilImg">
-                <img src={Geilson} alt="Geilson Freire" />
-            </div>
 
-            <p className="PerfilName">Geilson Freire</p>
 
-            <i className="MneuChevron" id="MneuChevron"><FaChevronDown  /></i>
+    return (
+        <div className="ContainerHeader">
+            <div className="InputBox">
+                <input type="text" placeholder="Pesaquise colleções" />
+                <i className="SearchIcon"><BiSearchAlt2 /></i>
+            </div> {/* Container do imput */}
 
-      </div> {/* Container do perfil */}
-    </div>
-  );
+            <div className="PerfilConatiner">
+
+                <i className="NotifiqueIcon"><FaBell /></i> {/* Icone de notificação */}
+
+                <div className="PerfilImg">
+                    <img src={Geilson} alt="Geilson Freire" />
+                </div> {/* Imagem do perfil */}
+
+                <p className="PerfilName">Geilson Freire</p> {/* Nome do perfil */}
+
+                <i className="MenuChevron" id="MenuChevron"><FaChevronDown /></i> {/* Icone de seta para baixo */}
+
+                <div className="MenuContainer" id="MenuConteiner">
+                    <ul>
+                        <li>Perfil</li>
+                        <li>Configurações</li>
+                        <li>Sair</li>
+                    </ul>
+                </div> {/* Container do menu drop*/}
+            </div> {/* Container do perfil */}
+        </div>
+    );
 }
 
 export default ContainerHeader; // Export the component
