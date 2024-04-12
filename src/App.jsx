cc7dea53011b77e356e055c3b6dc4a8bc7b26a4a
geilsonfreire <geilsonfreireleite@gmail.com>
2024-04-11 22:16:08 -0300

    Foi criado a barra de menu lateral e aplicado dos icones, foi estilizado em CSS e aplicado MidiaQuery

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import "./App.css"; // Importing CSS file
 import Menu from "./Components/Menu"; // Importing Menu component
 
 function App() {
-  return <div className="App">
-    <Menu />
-  </div>
+  return (
+    <div className="App">
+        <Menu />
+    </div>
+  );
 } 
 
 export default App; // Exporting App component
