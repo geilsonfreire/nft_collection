bfd91d83d18ebf113142eea1167858fc177baa22
geilsonfreire <geilosnfreireleite@gmail.com>
2024-10-20 14:56:45 -0300

    Foi removido todos os arquivos do projeto, deixando o repositorio limpo!

--- src/App.test.jsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';  // Importa os métodos render e screen
-import App from './App'; // Importa o componente App
-
-// Define um caso de teste chamado 'renders learn react link'
-test('renders learn react link', () => {
-  render(<App />); // Renderiza o componente App
-  const linkElement = screen.getByText(/learn react/i); // Busca o elemento que contém o texto 'learn react'
-  expect(linkElement).toBeInTheDocument(); // Verifica se o elemento foi renderizado
-}); 
